7d6cd198f4342ac593e034dcb15d8743cf8a9167
yagya <yagyagoel87@gmail.com>
2024-04-03 16:59:14 +0530

    added the signup route also added the api route

---
+++ app/api/user/route.ts
@@ -0,0 +1,6 @@
+export function GET(){
+    return Response.json({
+        name: "yagya",
+        email : "yagyagooe@gmail.com"
+    })
+}
---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+
+
+const Loading = () => {
+  return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            Loading...
+        </div>
+    </div>
+  )
+}
+
+export default Loading
