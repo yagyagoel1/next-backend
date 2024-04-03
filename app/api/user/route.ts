2fdc935e4f4eeb33416950c6d0ccbb7b5e2db654
yagya <yagyagoel87@gmail.com>
2024-04-03 17:56:30 +0530

    addded the backned route for storing the data

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,10 +1,20 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest } from "next/server";
+
+const client = new PrismaClient();
+
 
 export async  function POST(req:NextRequest){
     const body = await req.json();
 
-    console.log(body)
+    const user =await   client.user.create({
+        data:{
+            username : body.username,
+            password : body.password
 
+        }
+    })
+    console.log(user.id)
     return Response.json({
         message:"you are logged in "
     })
